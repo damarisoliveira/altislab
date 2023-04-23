25bdc7b27cd9edc0c145bf67555329036243c212
Damaris <damarisoliveira@users.noreply.github.com>
2023-04-23 16:57:19 -0300

    Inserindo pagina de projeto

--- javascript/projects.js
+++ javascript/projects.js
@@ -2,3 +2,12 @@ $('#filter').click(function(){
     const accordion = $(this).find('.accordion');
     accordion.toggleClass('active');
 });
+
+
+
+
+$('.project').click(function(){
+    $('.main').hide();
+    $('#project-info').show();
+});
+
